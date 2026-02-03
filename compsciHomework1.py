2220d7e74d9d38d499cb1139120abb63337aab16
morse15 <morsethe15th@gmail.com>
2026-02-03 20:37:07 +0100

    first homework

---
+++ compsciHomework1.py
@@ -0,0 +1,25 @@
+#make a list of five locations in unalphabetical order
+#print list
+#print list in alphabetical order without permanently changing the list
+#print original list
+#print in reverse alphabetical order without permanent change
+#print original list
+#reverse the list and print it
+#reverse again, print again
+#sort in alphabetical order, permanently
+#sort in reverse alphabetical order, permanently
+
+places = ["paris","lagos","mecca","abuja","milan"]
+print (places)
+print (sorted(places)) 
+print (places)
+print (sorted(places, reverse = True))
+print (places)
+places.reverse()
+print (places)
+places.reverse()
+print (places)
+places.sort()
+print (places)
+places.reverse()
+print (places)
